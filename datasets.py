2e6e311c4bc6b35f98b8b2fa23f0fe98ee57e0c5
re4rk <jois0157@gmail.com>
2022-12-24 19:52:17 +0900

    .

--- datasets.py
+++ datasets.py
@@ -11,69 +11,82 @@ import numpy as np
 import glob
 import argparse
 
-
+# get folders in the folder
+def get_folders(path):
+    folders = []
+    for name in os.listdir(path):
+        if os.path.isdir(os.path.join(path, name)):
+            folders.append(name)
+    return folders
 
 class_ids ={'man':0,'other':1}
-def create_meta(files_list,store_loc,mode='train'):
+
+def save_meta_file(file_path, meta_file_path):
+    fid = open(meta_file_path,'w')
+    for filepath in file_path:
+        fid.write(filepath+'\n')
+    fid.close()
+
+def create_meta(files_list, store_loc, mode='train', user_id='id10300'):
     if not os.path.exists(store_loc):
         os.makedirs(store_loc)
     
-    if mode=='train':
-        meta_store = store_loc+'/training.txt'
-        fid = open(meta_store,'w')
-        for filepath in files_list:
-            fid.write(filepath+'\n')
-        fid.close()
-    elif mode=='test':
-        meta_store = store_loc+'/testing.txt'
-        fid = open(meta_store,'w')
-        for filepath in files_list:
-            fid.write(filepath+'\n')
-        fid.close()
-    elif mode=='validation':
-        meta_store = store_loc+'/validation.txt'
-        fid = open(meta_store,'w')
-        for filepath in files_list:
-            fid.write(filepath+'\n')
-        fid.close()
-    else:
+    # make user specific folders in the meta folder
+    user_loc = os.path.join(store_loc, user_id)
+    if not os.path.exists(user_loc):
+        os.makedirs(user_loc)
+
+    try:
+        if mode=='train':
+            meta_store = os.path.join(user_loc, 'training.txt')
+        elif mode=='test':
+            meta_store = os.path.join(user_loc, 'testing.txt')
+        elif mode=='validation':
+            meta_store = os.path.join(user_loc, 'validation.txt')
+        save_meta_file(files_list, meta_store)
+    except Exception as e:
         print('Error in creating meta files')
-    
-def extract_files(folder_path):
+        print(e)
+
+def save_selected_file(nums, sub_files, label, train_lists):
+    for i in nums:
+        to_write = sub_files[i]+' '+str(class_ids[label])
+        train_lists.append(to_write)
+    return train_lists
+
+def extract_files(folder_path, user_id):
     all_lang_folders = sorted(glob.glob(folder_path+'/*/'))
     train_lists=[]
     test_lists = []
     val_lists=[]
     
     for lang_folderpath in all_lang_folders:
-        if "id10300" in lang_folderpath:
+        if user_id in lang_folderpath:
             language = "man"
         else :
             language = "other"
 
         sub_folders = glob.glob(lang_folderpath+'/*/')
-        train_nums = len(sub_folders)-int(len(sub_folders)*0.1)-int(len(sub_folders)*0.05)
-        for i in range(train_nums):
-            sub_folder = sub_folders[i]
-            all_files = sorted(glob.glob(sub_folder+'/*.wav'))
-            for audio_filepath in all_files:
-                to_write = audio_filepath+' '+str(class_ids[language])
-                train_lists.append(to_write)
-                
-        for i in range(train_nums,train_nums+int(len(sub_folders)*0.05)):
-            sub_folder = sub_folders[i]
-            all_files = sorted(glob.glob(sub_folder+'/*.wav'))
-            for audio_filepath in all_files:
-                to_write = audio_filepath+' '+str(class_ids[language])
-                val_lists.append(to_write)
+
+        files = []
+        for sub_folder in sub_folders:
+            all_files = glob.glob(sub_folder+'/*.wav')
+            files.extend(all_files)
         
-        for i in range(train_nums+int(len(sub_folders)*0.05),len(sub_folders)):
-            sub_folder = sub_folders[i]
-            all_files = sorted(glob.glob(sub_folder+'/*.wav'))
-            for audio_filepath in all_files:
-                to_write = audio_filepath+' '+str(class_ids[language])
-                test_lists.append(to_write)
-    return train_lists,test_lists,val_lists
+        np.random.shuffle(files)
+
+        zero_to_train = len(sub_folders) - int(len(sub_folders)*0.4)
+        train_nums = range(zero_to_train)
+        train_lists = save_selected_file(train_nums, files, language, train_lists)
+
+        train_to_val = len(sub_folders) - int(len(sub_folders)*0.2)
+        val_nums = range(zero_to_train, train_to_val)
+        val_lists = save_selected_file(val_nums, files, language, val_lists)
+
+        val_to_test = len(sub_folders)
+        test_nums = range(train_to_val, val_to_test)
+        test_lists = save_selected_file(test_nums, files, language, test_lists)
+    return train_lists, test_lists, val_lists
 
 
 if __name__ == '__main__':
@@ -81,9 +94,13 @@ if __name__ == '__main__':
     parser.add_argument("--processed_data", default="../vox1_test_wav", type=str,help='Dataset path')
     parser.add_argument("--meta_store_path", default="meta/", type=str,help='Save directory after processing')
     config = parser.parse_args()
-    train_list, test_list,val_lists = extract_files(config.processed_data)
 
-    create_meta(train_list,config.meta_store_path,mode='train')
-    create_meta(test_list,config.meta_store_path,mode='test')
-    create_meta(val_lists,config.meta_store_path,mode='validation')
-    
+    users = get_folders(config.processed_data)
+    print("users : ", end="")
+    print(users)
+    for user in users:
+        train_list, test_list,val_lists = extract_files(config.processed_data,user_id=user)
+
+        create_meta(train_list,config.meta_store_path,mode='train',user_id=user)
+        create_meta(test_list,config.meta_store_path,mode='test',user_id=user)
+        create_meta(val_lists,config.meta_store_path,mode='validation',user_id=user)
